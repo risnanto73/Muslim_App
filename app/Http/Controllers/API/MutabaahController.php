33107890eba5dd93eb75e0111abe22349c1cf10c
unknown <syatriarifqi@gmail.com>
2022-06-09 20:39:05 +0700

    push

--- app/Http/Controllers/API/MutabaahController.php
+++ app/Http/Controllers/API/MutabaahController.php
@@ -13,26 +13,15 @@ use Symfony\Component\HttpFoundation\Test\Constraint\ResponseIsRedirected;
 
 class MutabaahController extends Controller
 {
-    public function getMutabaah($id)
+    public function getMutabaah()
     {
 
-<<<<<<< HEAD
-        $user = User::where('id', $id)->first();
-        // $user = Auth::user();
+        $user = Auth::user();
         if (!$user) {
             return $this->responError(1, "Harap Login dulu");
         }
-        $mutabaah = Mutabaah::where('user_id','=', $user->id)->get();
-
-        // $mutabaah = Mutabaah::all();
-=======
-        $user = Auth::user();
-        // if (!$user) {
-        //     return $this->responError(1, "Harap Login dulu");
-        // }
 
         $mutabaah = Mutabaah::where('user_id', '=', $user->id)->get();
->>>>>>> d8f9d17c2d81f14cba7eb013a1147d6e270d72e6
         if (!$mutabaah) {
             return $this->responError(0, "data tidak tersedia");
         }
